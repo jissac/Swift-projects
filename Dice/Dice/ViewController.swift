b91dbada1cf5ed21e32a570b9fa4fff7a47036fb
James I <jissac@users.noreply.github.com>
2019-09-10 23:24:06 -0400

    dice app initial commit

---
+++ Dice/Dice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dice
+//
+//  Created by James Issac on 9/10/19.
+//  Copyright © 2019 Cloudburst Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
